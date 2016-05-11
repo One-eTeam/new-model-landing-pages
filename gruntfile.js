5287042c4e835e1f590a39e2fa0f01e5b4f04ece
Andrew Minion <andrew@andrewrminion.com>
2016-05-11 14:25:02 -0400

    add grunt copy for full-size images

--- gruntfile.js
+++ gruntfile.js
@@ -10,8 +10,19 @@ module.exports = function (grunt) {
           }]
       }
     },
+    copy: {
+      dev: {
+        files: [{
+          expand: true,
+          src: ['**/*{jpg,gif,png}'],
+          cwd: 'assets/',
+          dest: 'images/'
+        }]
+      }
+    }
   });
 
+  grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.registerTask('default', 'responsive_images');
+  grunt.registerTask('default', ['copy', 'responsive_images']);
 };
