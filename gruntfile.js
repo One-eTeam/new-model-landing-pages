1473ed3cc68a516a22df8070784b44bd55587541
Andrew Minion <andrew@andrewrminion.com>
2016-05-13 13:19:47 -0400

    add grunt juice and replace to make move to production easier

--- gruntfile.js
+++ gruntfile.js
@@ -19,10 +19,50 @@ module.exports = function (grunt) {
           dest: 'images/'
         }]
       }
-    }
+    },
+    juice: {
+      your_target: {
+        options: {
+          preserveMediaQueries: true,
+          applyAttributesTableElements: true,
+          applyWidthAttributes: true,
+          preserveImportant: true,
+          webResources: {
+            images: false
+          }
+        },
+        files: [{
+          expand: true,
+          src: '*.html',
+          dest: 'dist/'
+        }]
+      },
+    },
+    replace: {
+      options: {
+        usePrefix: false,
+        patterns: [
+          {
+            match: /images\//gi,  // Matches <img * src="images/ or <img * src='images/
+            replacement: 'https://one-eteam.github.io/new-model-landing-pages/images/'
+          },
+        ]
+      },
+      dist: {
+        files: [{
+          expand: true,
+          cwd: 'dist/',
+          src: ['*.html'],
+          dest: 'dist/'
+        }]
+      },
+    },
   });
 
   grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-juice');
+  grunt.loadNpmTasks('grunt-replace');
   grunt.loadNpmTasks('grunt-responsive-images');
-  grunt.registerTask('default', ['copy', 'responsive_images']);
+  grunt.registerTask('images', ['copy', 'responsive_images']);
+  grunt.registerTask('prep', ['juice', 'replace']);
 };
